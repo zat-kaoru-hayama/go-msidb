2255891ccda3637ed5ed5443777e1d21557edede
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-01-17 13:36:37 +0900

    msiver.exe: without arguments, show version and architecture

--- cmd/msiver/main.go
+++ cmd/msiver/main.go
@@ -4,13 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/zat-kaoru-hayama/go-msidb/internal/msiver"
 )
 
+var version string
+
 func main() {
 	flag.Parse()
-	if err := msiver.Show(flag.Args(), os.Stdout); err != nil {
+	args := flag.Args()
+	if len(args) <= 0 {
+		fmt.Fprintf(os.Stderr, "%s %s for %s/%s by %s\n",
+			os.Args[0], version, runtime.GOOS, runtime.GOARCH, runtime.Version())
+		flag.PrintDefaults()
+		return
+	}
+	if err := msiver.Show(args, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
